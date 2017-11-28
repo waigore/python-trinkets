18b1e71cec20c9fc9fa3de943329eacd7a7d8bb4
Kelvin Ho <kelvin.ho.ka.wai@gmail.com>
2017-11-28 22:10:28 +0800

    Add algo to compute rshifted arrays based on starter array state

--- nonosolve/nonosolve.py
+++ nonosolve/nonosolve.py
@@ -2,6 +2,7 @@ import json
 
 TYPE_ACROSS = 0
 TYPE_DOWN = 1
+
 SQ_EMPTY = ' '
 SQ_FILLED = '*'
 SQ_MARKED = 'X'
@@ -16,6 +17,22 @@ def format_col(col):
         print('[%s]' % val)
     print('\n')
 
+def rshift(arr, s, e, shift_by):
+    blocks_to_shift = e-s+1
+    for i in range(0, shift_by):
+        arr2 = list(arr)
+        for j in reversed(range(blocks_to_shift)):
+            fr = s+j
+            to = fr+i+1
+            arr2[to] = arr2[fr]
+            arr2[fr] = SQ_EMPTY
+        format_row(arr2)
+
+#test_arr = ['X', 'X', 'X', ' ', 'X', ' ', 'X', 'X', ' ', ' ']
+#rshift(test_arr, 6, 7, 2)
+#rshift(test_arr, 4, 7, 2)
+#rshift(test_arr, 0, 7, 2)
+
 class PossibleStates(object):
     def __init__(self, length, arr_values, typ):
         self.arr_values = arr_values
@@ -29,6 +46,29 @@ class PossibleStates(object):
         else:
             format_col(self.starter_arr)
         '''
+        self.gen_rshifted_arrays()
+
+    def is_solved(self, arr):
+        return SQ_EMPTY in arr
+
+    def gen_rshifted_arrays(self):
+        start_indexes = []
+        end_indexes = []
+        for i in range(len(self.arr_values)):
+            if i == 0:
+                start_indexes.append(0)
+                end_indexes.append(self.arr_values[0]-1)
+            else:
+                s_index = sum(self.arr_values[0:i])+i
+                e_index = s_index+self.arr_values[i]-1
+                start_indexes.append(s_index)
+                end_indexes.append(e_index)
+        #print(start_indexes, end_indexes)
+        shift_by = self.arr_values[-1]
+        e = end_indexes[-1]
+        for i in reversed(range(len(start_indexes))):
+            s = start_indexes[i]
+            #rshift(self.starter_arr, s, e, shift_by)
 
     def create_starter_arr(self):
         self.starter_arr = [SQ_EMPTY]*self.length
