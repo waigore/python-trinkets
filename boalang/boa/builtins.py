4f3fb00713a1cdff200f3c3fe200d2dbbb4cdbef
Kelvin Ho <kelvin.ho.ka.wai@gmail.com>
2019-05-12 12:32:26 +0800

    Builtin push function

--- boalang/boa/builtins.py
+++ boalang/boa/builtins.py
@@ -3,6 +3,8 @@ from .object import (
     newBuiltinFunction,
     newError,
     newInteger,
+    newArray,
+    newString,
     OBJECT_TYPES,
 )
 
@@ -45,6 +47,30 @@ def builtin_last(args):
     except:
         return newError("Sequence index error")
 
+def builtin_rest(args):
+    if len(args) != 1:
+        return newError("Wrong number of arguments to rest. Got %d, want 1" % len(args))
+    arg = args[0]
+
+    if arg.objectType == OBJECT_TYPES.OBJECT_TYPE_STRING:
+        return newString(arg.value[1:])
+    elif arg.objectType == OBJECT_TYPES.OBJECT_TYPE_ARRAY:
+        return newArray(arg.value[1:])
+    else:
+        return newError("Argument to last not supported. Got %s" % arg.objectType)
+
+def builtin_push(args):
+    if len(args) < 2:
+        return newError("Wrong number of arguments to push. Got %d, want >=2" % len(args))
+    arrObj = args[0]
+    objs = args[1:]
+
+    if arrObj.objectType != OBJECT_TYPES.OBJECT_TYPE_ARRAY:
+        return newError("Argument 1 to push must be array. Got %s" % arrObj.objectType)
+
+    newArrVal = arrObj.value + objs
+    return newArray(newArrVal)
+
 def builtin_print(args):
     if len(args) == 0:
         return newError("print expected at least 1 argument.")
@@ -55,5 +81,7 @@ BUILTIN_FUNCTIONS = {
     'len': newBuiltinFunction('len', builtin_len),
     'first': newBuiltinFunction('first', builtin_first),
     'last': newBuiltinFunction('last', builtin_last),
+    'rest': newBuiltinFunction('rest', builtin_rest),
+    'push': newBuiltinFunction('push', builtin_push),
     'print': newBuiltinFunction('print', builtin_print),
 }
